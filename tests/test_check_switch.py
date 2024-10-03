c943bdc9b7d70b03186b61d0ec43524a1efa98a3
Erik De Bonte <erikd@microsoft.com>
2024-10-02 19:19:46 -0700

    Unit test

---
+++ tests/test_check_switch.py
@@ -0,0 +1,33 @@
+import datetime
+
+from sarif.cmdline.main import _check
+from sarif import sarif_file
+
+SARIF = {
+  "runs": [
+    {
+      "tool": {
+        "driver": {
+          "name": "Tool"
+        }
+      },
+      "results": [
+        {
+          "level": "warning",
+          "ruleId": "rule"
+        }
+      ]
+    }
+  ]
+}
+
+
+def test_check():
+    fileSet = sarif_file.SarifFileSet()
+    fileSet.add_file(sarif_file.SarifFile("SARIF", SARIF))
+
+    result = _check(fileSet, "error")
+    assert result == 0
+
+    result = _check(fileSet, "warning")
+    assert result == 1
