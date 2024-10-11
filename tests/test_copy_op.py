00f284a86c4a873d740939f9f19c89ccafdf651d
Erik De Bonte <erikd@microsoft.com>
2024-10-11 15:29:06 -0700

    Extend copy test to validate output contents

--- tests/test_copy_op.py
+++ tests/test_copy_op.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 import datetime
 import json
 import jsonschema
@@ -41,24 +42,48 @@ def test_generate_sarif():
     sarif_schema_file = os.path.join(
         os.path.dirname(__file__), "sarif-schema-2.1.0.json"
     )
-    with open(sarif_schema_file, "rb") as f_in:
-        sarif_schema = json.load(f_in)
+    with open(sarif_schema_file, "rb") as f_schema:
+        sarif_schema = json.load(f_schema)
     input_sarif_file = sarif_file.SarifFile(
         "SARIF_WITH_1_ISSUE", SARIF_WITH_1_ISSUE, mtime=datetime.datetime.now()
     )
     jsonschema.validate(input_sarif_file.data, schema=sarif_schema)
+
     input_sarif_file_set = sarif_file.SarifFileSet()
     input_sarif_file_set.files.append(input_sarif_file)
     with tempfile.TemporaryDirectory() as tmp:
-        file_path = os.path.join(tmp, "copied.json")
+        ouput_file_path = os.path.join(tmp, "copied.json")
         output_sarif_file = copy_op.generate_sarif(
             input_sarif_file_set,
-            file_path,
+            ouput_file_path,
             append_timestamp=False,
             sarif_tools_version="1.2.3",
             cmdline="unit-test",
         )
-        with open(file_path, "rb") as f_in:
-            output_sarif = json.load(f_in)
+
+        with open(ouput_file_path, "rb") as f_out:
+            output_sarif = json.load(f_out)
         assert output_sarif_file.data == output_sarif
         jsonschema.validate(output_sarif, schema=sarif_schema)
+
+        expected_sarif = deepcopy(input_sarif_file.data)
+        conversion = {
+            "tool": {
+                "driver": {
+                    "name": "sarif-tools",
+                    "fullName": "sarif-tools https://github.com/microsoft/sarif-tools/",
+                    "version": "1.2.3",
+                    "properties": {
+                        "file": input_sarif_file.abs_file_path,
+                        "modified": input_sarif_file.mtime.isoformat(),
+                        "processed": output_sarif["runs"][0]["conversion"]["tool"]["driver"]["properties"]["processed"],
+                    },
+                }
+            },
+            "invocation": {
+                "commandLine": "unit-test",
+                "executionSuccessful": True,
+            },
+        }
+        expected_sarif["runs"][0]["conversion"] = conversion
+        assert output_sarif == expected_sarif
