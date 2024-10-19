bd10fc06f081966b72852d8ea2ffa1219211dd08
Erik De Bonte <erikd@microsoft.com>
2024-10-18 17:47:38 -0700

    Added blame tests

---
+++ tests/test_blame_op.py
@@ -0,0 +1,158 @@
+from copy import deepcopy
+import datetime
+import json
+import jsonschema
+import os
+import tempfile
+from typing import List
+
+from sarif.operations import blame_op
+from sarif import sarif_file
+from tests.utils import get_sarif_schema
+
+ERROR_FILE_RELATIVE_PATH = "subdir/file.py"
+ERROR_FILE_ABSOLUTE_PATH = "file:///C:/repo/subdir/file.py"
+
+SARIF_FILE = {
+    "$schema": "https://schemastore.azurewebsites.net/schemas/json/sarif-2.1.0-rtm.5.json",
+    "version": "2.1.0",
+    "runs": [
+        {
+            "tool": {"driver": {"name": "unit test"}},
+            "results": [
+                {
+                    "ruleId": "CA2101",
+                    "message": {"text": "just testing"},
+                    "level": "error",
+                    "locations": [
+                        {
+                            "physicalLocation": {
+                                "artifactLocation": {
+                                    "uri": ERROR_FILE_ABSOLUTE_PATH,
+                                    "index": 0,
+                                },
+                                "region": {"startLine": 3, "startColumn": 9},
+                            }
+                        }
+                    ],
+                }
+            ],
+        }
+    ],
+}
+
+GIT_BLAME_OUTPUT = [
+    "f9db03438aba52affc5c3fcdb619afa620ad603a 1 1 7\n",
+    "author Taylor Developer\n",
+    "author-mail <taylor@developer.com>\n",
+    "author-time 1699272533\n",
+    "author-tz +0000\n",
+    "committer GitHub\n",
+    "committer-mail <noreply@github.com>\n",
+    "committer-time 1699272533\n",
+    "committer-tz +0000\n",
+    "summary Commit message 1\n",
+    "filename " + ERROR_FILE_RELATIVE_PATH + "\n",
+    "\tFile text line 1\n",
+    "f9db03438aba52affc5c3fcdb619afa620ad603a 2 2\n",
+    "\tFile text line 2\n",
+    "f9db03438aba52affc5c3fcdb619afa620ad603a 3 3\n",
+    "\tFile text line 3\n",
+    "eec0471db074a037d820abdda1f210f8a8c987ca 4 4 1\n",
+    "author Other Developer\n",
+    "author-mail <other@developer.com>\n",
+    "author-time 1718035364\n",
+    "author-tz +0100\n",
+    "committer GitHub\n",
+    "committer-mail <noreply@github.com>\n",
+    "committer-time 1718035364\n",
+    "committer-tz +0100\n",
+    "summary Commit message 2\n",
+    "filename " + ERROR_FILE_RELATIVE_PATH + "\n",
+    "\tFile text line 4\n",
+    "6732313c320314c122bd00aa40e7c79954f21c15 5 5 1\n",
+    "author Another Developer\n",
+    "author-mail <another@developer.com>\n",
+    "author-time 1727710690\n",
+    "author-tz -0700\n",
+    "committer GitHub\n",
+    "committer-mail <noreply@github.com>\n",
+    "committer-time 1727710690\n",
+    "committer-tz -0700\n",
+    "summary Commit message 3\n",
+    "filename " + ERROR_FILE_RELATIVE_PATH + "\n",
+    "\tFile text line 5\n",
+    "6732313c320314c122bd00aa40e7c79954f21c15 6 6\n",
+    "\tFile text line 6\n",
+]
+
+
+def test_blame_no_blame_info():
+    input_sarif_file = sarif_file.SarifFile(
+        "SARIF_FILE", SARIF_FILE, mtime=datetime.datetime.now()
+    )
+    input_sarif_file_set = sarif_file.SarifFileSet()
+    input_sarif_file_set.files.append(input_sarif_file)
+
+    with tempfile.TemporaryDirectory() as tmp:
+        repo_path = os.path.join(tmp, "repo")
+        os.makedirs(repo_path)
+        output_file_path = os.path.join(tmp, "blamed.json")
+
+        blame_op.enhance_with_blame(
+            input_sarif_file_set,
+            repo_path,
+            output_file_path,
+            output_multiple_files=False,
+            run_git_blame=lambda repo_path, file_path: [],
+        )
+
+        assert not os.path.isfile(output_file_path)
+
+
+def test_blame_success():
+    input_sarif_file = sarif_file.SarifFile(
+        "SARIF_FILE", SARIF_FILE, mtime=datetime.datetime.now()
+    )
+    input_sarif_file_set = sarif_file.SarifFileSet()
+    input_sarif_file_set.files.append(input_sarif_file)
+
+    with tempfile.TemporaryDirectory() as tmp:
+        repo_path = os.path.join(tmp, "repo")
+        os.makedirs(repo_path)
+        output_file_path = os.path.join(tmp, "blamed.json")
+
+        def run_git_blame(blame_repo_path: str, blame_file_path: str) -> List[bytes]:
+            assert blame_repo_path == repo_path
+            assert blame_file_path == ERROR_FILE_ABSOLUTE_PATH
+            return [x.encode() for x in GIT_BLAME_OUTPUT]
+
+        blame_op.enhance_with_blame(
+            input_sarif_file_set,
+            repo_path,
+            output_file_path,
+            output_multiple_files=False,
+            run_git_blame=run_git_blame,
+        )
+
+        with open(output_file_path, "rb") as f_out:
+            output_sarif = json.load(f_out)
+        jsonschema.validate(output_sarif, schema=get_sarif_schema())
+
+        expected_sarif = deepcopy(input_sarif_file.data)
+        blame_properties = {
+            "blame": {
+                "author": "Taylor Developer",
+                "author-mail": "<taylor@developer.com>",
+                "author-time": "1699272533",
+                "author-tz": "+0000",
+                "committer": "GitHub",
+                "committer-mail": "<noreply@github.com>",
+                "committer-time": "1699272533",
+                "committer-tz": "+0000",
+                "summary": "Commit message 1",
+                "filename": ERROR_FILE_RELATIVE_PATH,
+            }
+        }
+        expected_sarif["runs"][0]["results"][0]["properties"] = blame_properties
+        assert output_sarif == expected_sarif
--- tests/test_copy_op.py
+++ tests/test_copy_op.py
@@ -5,8 +5,10 @@ import jsonschema
 import os
 import tempfile
 
+import sarif
 from sarif.operations import copy_op
 from sarif import sarif_file
+from tests.utils import get_sarif_schema
 
 SARIF_WITH_1_ISSUE = {
     "$schema": "https://schemastore.azurewebsites.net/schemas/json/sarif-2.1.0-rtm.5.json",
@@ -38,12 +40,7 @@ SARIF_WITH_1_ISSUE = {
 
 
 def test_generate_sarif():
-    # JSON Schema file for SARIF obtained from https://docs.oasis-open.org/sarif/sarif/v2.1.0/cs01/schemas/
-    sarif_schema_file = os.path.join(
-        os.path.dirname(__file__), "sarif-schema-2.1.0.json"
-    )
-    with open(sarif_schema_file, "rb") as f_schema:
-        sarif_schema = json.load(f_schema)
+    sarif_schema = get_sarif_schema()
     input_sarif_file = sarif_file.SarifFile(
         "SARIF_WITH_1_ISSUE", SARIF_WITH_1_ISSUE, mtime=datetime.datetime.now()
     )
@@ -52,16 +49,16 @@ def test_generate_sarif():
     input_sarif_file_set = sarif_file.SarifFileSet()
     input_sarif_file_set.files.append(input_sarif_file)
     with tempfile.TemporaryDirectory() as tmp:
-        ouput_file_path = os.path.join(tmp, "copied.json")
+        output_file_path = os.path.join(tmp, "copied.json")
         output_sarif_file = copy_op.generate_sarif(
             input_sarif_file_set,
-            ouput_file_path,
+            output_file_path,
             append_timestamp=False,
             sarif_tools_version="1.2.3",
             cmdline="unit-test",
         )
 
-        with open(ouput_file_path, "rb") as f_out:
+        with open(output_file_path, "rb") as f_out:
             output_sarif = json.load(f_out)
         assert output_sarif_file.data == output_sarif
         jsonschema.validate(output_sarif, schema=sarif_schema)
@@ -76,7 +73,9 @@ def test_generate_sarif():
                     "properties": {
                         "file": input_sarif_file.abs_file_path,
                         "modified": input_sarif_file.mtime.isoformat(),
-                        "processed": output_sarif["runs"][0]["conversion"]["tool"]["driver"]["properties"]["processed"],
+                        "processed": output_sarif["runs"][0]["conversion"]["tool"][
+                            "driver"
+                        ]["properties"]["processed"],
                     },
                 }
             },
