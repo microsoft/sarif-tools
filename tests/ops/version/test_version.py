f8f381028cf2d4dfc809b9d06ddde7619ba920d4
Erik De Bonte <erikd@microsoft.com>
2024-12-10 17:34:18 -0800

    Move op tests into separate dirs

--- tests/ops/version/test_version.py
+++ tests/ops/version/test_version.py
@@ -4,7 +4,9 @@ import sarif
 
 
 def test_version():
-    with open(pathlib.Path(__file__).parent.parent / "pyproject.toml") as pyproject_in:
+    with open(
+        pathlib.Path(__file__).parent.parent.parent.parent / "pyproject.toml"
+    ) as pyproject_in:
         for pyproject_line in pyproject_in.readlines():
             if pyproject_line.startswith('version = "'):
                 assert pyproject_line.strip() == f'version = "{sarif.__version__}"'
